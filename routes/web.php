d106b0217118643ff25200ca8a4bea296bdec12e
Ilya <iveliniu@yandex.ru>
2021-09-10 00:23:37 +0500

    Закончено создание страницы сертификаты1

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\AdminPanel\Content\ContentSheetTimelinePageController;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes111111111
+| Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
